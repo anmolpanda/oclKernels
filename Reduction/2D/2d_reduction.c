1b3901b81f1847c453f195caca4ae371445213bc
leiming <leimingyu830@gmail.com>
2013-10-17 18:39:13 -0400

    2d reduction,nyan

--- Reduction/2D/2d_reduction.c
+++ Reduction/2D/2d_reduction.c
@@ -10,13 +10,15 @@
 #include "../../Utils/c_utils.h"
 #include "../../Utils/ocl_utils.h"
 
+#define TILE 64
 
 //-----------------------------
 //    A(NxT)  *  B (TxN) = C
 //-----------------------------
 
-void run1(int N);
-//void run2(int N, int T);
+//void run1(int N); // one kernel
+//void run2(int N); // two kernels
+void run3(int N);
 
 
 int main (int argc, char *argv[])
@@ -28,7 +30,13 @@ int main (int argc, char *argv[])
 
 	int N = atoi(argv[1]); // row
 
-	run1(N);
+	//run1(N);
+	//
+	
+	//run2(N);
+	//
+	
+	run3(N);
 
 	//run_opt(N,T);
 
@@ -37,9 +45,11 @@ int main (int argc, char *argv[])
 	return  0;
 }
 
-void run1(int N)
+/*
+void run2(int N)
 {
 	puts("lauching 1d threads");
+
 
 	float *A;
 	A = (float*)malloc(sizeof(float)*N*N);
@@ -53,8 +63,222 @@ void run1(int N)
 	float *C;
 	C = (float*)malloc(sizeof(float)*N*N);
 
+	int blks = (N+63)/64;
+
 	float *sum;
-	sum = (float*)malloc(sizeof(float));
+	sum = (float*)malloc(sizeof(float)*(blks+1));
+
+
+
+	int NumK = 2;
+	int NumE = 2;
+
+	int i;
+
+	double gpuTime;
+	cl_ulong gstart, gend;
+
+	//------------------------------------------------
+	//  OpenCL 
+	//------------------------------------------------
+	cl_int err;
+
+	cl_platform_id platform;          // OpenCL platform
+	cl_device_id device_id;           // device ID
+	cl_context context;               // context
+	cl_command_queue queue;           // command queue
+	cl_program program;               // program
+
+	cl_kernel *kernel = (cl_kernel*)malloc(sizeof(cl_kernel)*NumK);
+
+	cl_event *event = (cl_event*)malloc(sizeof(cl_event)*NumE);    
+
+	// read kernel file
+	char *fileName = "kernel.cl";
+	char *kernelSource;
+	size_t size;
+	FILE *fh = fopen(fileName, "rb");
+	if(!fh) {
+		printf("Error: Failed to open kernel file!\n");
+		exit(1);
+	}
+	fseek(fh,0,SEEK_END);
+	size=ftell(fh);
+	fseek(fh,0,SEEK_SET);
+	kernelSource = malloc(size+1);
+	size_t result;
+	result = fread(kernelSource,1,size,fh);
+	if(result != size){ fputs("Reading error", stderr);exit(1);}
+	kernelSource[size] = '\0';
+	
+	// Bind to platform
+	err = clGetPlatformIDs(1, &platform, NULL);
+	OCL_CHECK(err);
+
+	// Get ID for the device
+	err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
+	OCL_CHECK(err);
+
+	// Create a context  
+	context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
+	OCL_CHECK(err);
+
+	// Create a command queue 
+	queue = clCreateCommandQueue(context, device_id, CL_QUEUE_PROFILING_ENABLE, &err);
+	OCL_CHECK(err);
+
+	// Create the compute program from the source buffer
+	program = clCreateProgramWithSource(context, 1, (const char **)&kernelSource, NULL, &err);
+	OCL_CHECK(err);
+
+	// turn on optimization for kernel
+	char *options="-cl-mad-enable -cl-fast-relaxed-math -cl-no-signed-zeros -cl-unsafe-math-optimizations -cl-finite-math-only";
+
+	err = clBuildProgram(program, 1, &device_id, options, NULL, NULL);
+	if(err != CL_SUCCESS)
+		printCompilerOutput(program, device_id);
+	OCL_CHECK(err);
+
+	kernel[0] = clCreateKernel(program, "reduction_2a", &err);
+	OCL_CHECK(err);
+
+	kernel[1] = clCreateKernel(program, "reduction_2b", &err);
+	OCL_CHECK(err);
+
+	// memory on device
+	cl_mem A_d   = clCreateBuffer(context, CL_MEM_READ_WRITE,  sizeof(float)*N*N,  NULL, NULL);
+	//cl_mem C_d   = clCreateBuffer(context, CL_MEM_READ_WRITE,  sizeof(float)*N*N,  NULL, NULL);
+	cl_mem sum_d = clCreateBuffer(context, CL_MEM_READ_WRITE,  sizeof(float)*(blks+1),      NULL, NULL);
+
+	// Initialize device memory
+	err = clEnqueueWriteBuffer(queue, A_d, 	CL_TRUE, 0, sizeof(float)*N*N, 	A, 0, NULL, &event[0]); 
+	OCL_CHECK(err);
+
+	size_t localsize;
+	size_t globalsize;
+
+	localsize = 64;
+	globalsize = ((N+63)/64)*64;
+
+	err  = clSetKernelArg(kernel[0], 0, sizeof(cl_mem), &A_d);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	//err  = clSetKernelArg(kernel[0], 1, sizeof(cl_mem), &C_d);
+	//if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	err  = clSetKernelArg(kernel[0], 1, sizeof(cl_mem), &sum_d);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	err  = clSetKernelArg(kernel[0], 2, sizeof(float)*64, NULL);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	err  = clSetKernelArg(kernel[0], 3, sizeof(int), &N);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	err  = clSetKernelArg(kernel[0], 4, sizeof(int), &blks);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	size_t k2_localsize;
+	size_t k2_globalsize;
+
+	k2_localsize = 64;
+	k2_globalsize = ((blks+63)/64)*64;
+
+	err  = clSetKernelArg(kernel[1], 0, sizeof(cl_mem), &sum_d);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	err  = clSetKernelArg(kernel[1], 1, sizeof(float)*64, NULL);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	err  = clSetKernelArg(kernel[1], 2, sizeof(int), &N);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	err  = clSetKernelArg(kernel[1], 3, sizeof(int), &blks);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+
+	err = clEnqueueNDRangeKernel(queue, kernel[0], 1, NULL, &globalsize, &localsize, 0, NULL, NULL);
+	OCL_CHECK(err);
+
+	err = clEnqueueNDRangeKernel(queue, kernel[1], 1, NULL, &k2_globalsize, &k2_localsize, 0, NULL, NULL);
+
+
+	clFinish(queue);
+
+	clEnqueueReadBuffer(queue, sum_d, CL_TRUE, 0, sizeof(float)*(blks+1), sum, 0, NULL , &event[1]);
+
+	err = clWaitForEvents(1,&event[1]);
+	OCL_CHECK(err);
+
+	err = clGetEventProfilingInfo (event[0], CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &gstart, NULL);
+	OCL_CHECK(err);
+
+	err = clGetEventProfilingInfo (event[1], CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &gend, NULL);
+	OCL_CHECK(err);
+
+	gpuTime = (double)(gend -gstart)/1000000000.0;
+
+	printf("oclTime = %lf (s)\n", gpuTime );
+
+
+	//check_1d_f(sum, blks+1);
+
+#ifdef DEBUG
+	//puts("C");
+	//check_2d_f(C,N,N);
+
+#endif
+
+
+	// free
+	clReleaseMemObject(A_d);	
+	//clReleaseMemObject(C_d);
+	clReleaseMemObject(sum_d);
+
+
+	clReleaseProgram(program);
+	clReleaseContext(context);
+	clReleaseCommandQueue(queue);
+	for(i=0;i<NumK;++i){
+		clReleaseKernel(kernel[i]);
+	}
+	for(i=0;i<NumE;++i){
+		clReleaseEvent(event[i]);
+	}
+	free(kernelSource);
+
+
+
+	free(A);
+	free(C);
+	free(sum);
+
+	return;
+}
+*/
+
+/*
+void run1(int N)
+{
+	puts("run 1\n");
+
+
+	float *A;
+	A = (float*)malloc(sizeof(float)*N*N);
+	init_2d_f(A,N,N,1.f);
+
+#ifdef DEBUG
+	puts("A");
+	check_2d_f(A,N,N);
+#endif
+
+	//float *C;
+	//C = (float*)malloc(sizeof(float)*N*N);
+
+	int blks = (N+63)/64;
+
+	float *sum;
+	sum = (float*)malloc(sizeof(float)*(blks+1));
 
 	int NumK = 1;
 	int NumE = 2;
@@ -131,8 +355,8 @@ void run1(int N)
 
 	// memory on device
 	cl_mem A_d   = clCreateBuffer(context, CL_MEM_READ_WRITE,  sizeof(float)*N*N,  NULL, NULL);
-	cl_mem C_d   = clCreateBuffer(context, CL_MEM_READ_WRITE,  sizeof(float)*N*N,  NULL, NULL);
-	cl_mem sum_d = clCreateBuffer(context, CL_MEM_READ_WRITE,  sizeof(float),      NULL, NULL);
+//	cl_mem C_d   = clCreateBuffer(context, CL_MEM_READ_WRITE,  sizeof(float)*N*N,  NULL, NULL);
+	cl_mem sum_d = clCreateBuffer(context, CL_MEM_READ_WRITE,  sizeof(float)*(blks+1),      NULL, NULL);
 
 	// Initialize device memory
 	err = clEnqueueWriteBuffer(queue, A_d, 	CL_TRUE, 0, sizeof(float)*N*N, 	A, 0, NULL, &event[0]); 
@@ -147,21 +371,249 @@ void run1(int N)
 	err  = clSetKernelArg(kernel[0], 0, sizeof(cl_mem), &A_d);
 	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
 
-	err  = clSetKernelArg(kernel[0], 1, sizeof(cl_mem), &C_d);
+	//err  = clSetKernelArg(kernel[0], 1, sizeof(cl_mem), &C_d);
+	//if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	err  = clSetKernelArg(kernel[0], 1, sizeof(cl_mem), &sum_d);
 	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
 
-	err  = clSetKernelArg(kernel[0], 2, sizeof(cl_mem), &sum_d);
+	err  = clSetKernelArg(kernel[0], 2, sizeof(float)*64, NULL);
 	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
 
-	err  = clSetKernelArg(kernel[0], 3, sizeof(float)*64, NULL);
+	err  = clSetKernelArg(kernel[0], 3, sizeof(int), &N);
 	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
 
-	err  = clSetKernelArg(kernel[0], 4, sizeof(int), &N);
+	err  = clSetKernelArg(kernel[0], 4, sizeof(int), &blks);
 	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
 
 
 	err = clEnqueueNDRangeKernel(queue, kernel[0], 1, NULL, &globalsize, &localsize, 0, NULL, NULL);
 	OCL_CHECK(err);
+
+	clFinish(queue);
+
+	clEnqueueReadBuffer(queue, sum_d, CL_TRUE, 0, sizeof(float)*(blks+1), sum, 0, NULL , &event[1]);
+
+	err = clWaitForEvents(1,&event[1]);
+	OCL_CHECK(err);
+
+	err = clGetEventProfilingInfo (event[0], CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &gstart, NULL);
+	OCL_CHECK(err);
+
+	err = clGetEventProfilingInfo (event[1], CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &gend, NULL);
+	OCL_CHECK(err);
+
+	gpuTime = (double)(gend -gstart)/1000000000.0;
+
+	printf("oclTime = %lf (s)\n", gpuTime );
+
+
+	//check_1d_f(sum, blks+1);
+
+#ifdef DEBUG
+	//puts("C");
+	//check_2d_f(C,N,N);
+
+#endif
+
+
+	// free
+	clReleaseMemObject(A_d);	
+	//clReleaseMemObject(C_d);
+	clReleaseMemObject(sum_d);
+
+
+	clReleaseProgram(program);
+	clReleaseContext(context);
+	clReleaseCommandQueue(queue);
+	for(i=0;i<NumK;++i){
+		clReleaseKernel(kernel[i]);
+	}
+	for(i=0;i<NumE;++i){
+		clReleaseEvent(event[i]);
+	}
+	free(kernelSource);
+
+
+
+	free(A);
+	//free(C);
+	free(sum);
+
+	return;
+}
+
+*/
+
+void run3(int N)
+{
+	puts("run 3\n");
+
+
+	float *A;
+	A = (float*)malloc(sizeof(float)*N*N);
+	init_2d_f(A,N,N,1.f);
+
+#ifdef DEBUG
+	puts("A");
+	check_2d_f(A,N,N);
+#endif
+
+	//float *C;
+	//C = (float*)malloc(sizeof(float)*N*N);
+
+	int blks = (N+TILE-1)/TILE;
+
+	float *sum;
+	sum = (float*)malloc(sizeof(float));
+
+	int NumK = 1;
+	int NumE = 2;
+
+	int i;
+
+	double gpuTime;
+	cl_ulong gstart, gend;
+
+	//------------------------------------------------
+	//  OpenCL 
+	//------------------------------------------------
+	cl_int err;
+
+	cl_platform_id platform;          // OpenCL platform
+	cl_device_id device_id;           // device ID
+	cl_context context;               // context
+	cl_command_queue queue;           // command queue
+	cl_program program;               // program
+
+	cl_kernel *kernel = (cl_kernel*)malloc(sizeof(cl_kernel)*NumK);
+
+	cl_event *event = (cl_event*)malloc(sizeof(cl_event)*NumE);    
+
+	// read kernel file
+	char *fileName = "kernel.cl";
+	char *kernelSource;
+	size_t size;
+	FILE *fh = fopen(fileName, "rb");
+	if(!fh) {
+		printf("Error: Failed to open kernel file!\n");
+		exit(1);
+	}
+	fseek(fh,0,SEEK_END);
+	size=ftell(fh);
+	fseek(fh,0,SEEK_SET);
+	kernelSource = malloc(size+1);
+	size_t result;
+	result = fread(kernelSource,1,size,fh);
+	if(result != size){ fputs("Reading error", stderr);exit(1);}
+	kernelSource[size] = '\0';
+	
+	// Bind to platform
+	err = clGetPlatformIDs(1, &platform, NULL);
+	OCL_CHECK(err);
+
+	// Get ID for the device
+	err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
+	OCL_CHECK(err);
+
+	// Create a context  
+	context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
+	OCL_CHECK(err);
+
+	// Create a command queue 
+	queue = clCreateCommandQueue(context, device_id, CL_QUEUE_PROFILING_ENABLE, &err);
+	OCL_CHECK(err);
+
+	// Create the compute program from the source buffer
+	program = clCreateProgramWithSource(context, 1, (const char **)&kernelSource, NULL, &err);
+	OCL_CHECK(err);
+
+	// turn on optimization for kernel
+	char *options="-cl-mad-enable -cl-fast-relaxed-math -cl-no-signed-zeros -cl-unsafe-math-optimizations -cl-finite-math-only";
+
+	err = clBuildProgram(program, 1, &device_id, options, NULL, NULL);
+	if(err != CL_SUCCESS)
+		printCompilerOutput(program, device_id);
+	OCL_CHECK(err);
+
+	kernel[0] = clCreateKernel(program, "reduction_3a", &err);
+	OCL_CHECK(err);
+
+	kernel[1] = clCreateKernel(program, "reduction_3b", &err);
+	OCL_CHECK(err);
+
+	// memory on device
+	cl_mem A_d   = clCreateBuffer(context, CL_MEM_READ_WRITE,  sizeof(float)*N*N,  NULL, NULL);
+//	cl_mem C_d   = clCreateBuffer(context, CL_MEM_READ_WRITE,  sizeof(float)*N*N,  NULL, NULL);
+	cl_mem intersum_d = clCreateBuffer(context, CL_MEM_READ_WRITE,  sizeof(float)*N,      NULL, NULL);
+
+	cl_mem sum_d = clCreateBuffer(context, CL_MEM_READ_WRITE,  sizeof(float),      NULL, NULL);
+
+	// Initialize device memory
+	err = clEnqueueWriteBuffer(queue, A_d, 	CL_TRUE, 0, sizeof(float)*N*N, 	A, 0, NULL, &event[0]); 
+	OCL_CHECK(err);
+
+
+	// kernel a
+	size_t localsize[2];
+	size_t globalsize[2];
+
+	localsize[0] = 1;
+	localsize[1] = TILE;
+
+	globalsize[0]= N;
+	globalsize[1]= TILE;
+
+	err  = clSetKernelArg(kernel[0], 0, sizeof(cl_mem), &A_d);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+	//err  = clSetKernelArg(kernel[0], 1, sizeof(cl_mem), &C_d);
+	//if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+	err  = clSetKernelArg(kernel[0], 1, sizeof(cl_mem), &intersum_d);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	err  = clSetKernelArg(kernel[0], 2, sizeof(float)*TILE, NULL);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	err  = clSetKernelArg(kernel[0], 3, sizeof(int), &N);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	err  = clSetKernelArg(kernel[0], 4, sizeof(int), &blks);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+
+	// kernel b
+	size_t k2_localsize;
+	size_t k2_globalsize;
+
+	k2_localsize = TILE;
+	k2_globalsize= blks* TILE * 0.5;
+
+	err  = clSetKernelArg(kernel[1], 0, sizeof(cl_mem), &intersum_d);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	err  = clSetKernelArg(kernel[1], 1, sizeof(cl_mem), &sum_d);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	err  = clSetKernelArg(kernel[1], 2, sizeof(float)*TILE, NULL);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	err  = clSetKernelArg(kernel[1], 3, sizeof(int), &N);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	err  = clSetKernelArg(kernel[1], 4, sizeof(int), &blks);
+	if(err != 0) { printf("%d\n",err); OCL_CHECK(err); exit(1);}
+
+	err = clEnqueueNDRangeKernel(queue, kernel[0], 2, NULL, globalsize, localsize, 0, NULL, NULL);
+	OCL_CHECK(err);
+
+	err = clEnqueueNDRangeKernel(queue, kernel[1], 1, NULL, &k2_globalsize, &k2_localsize, 0, NULL, NULL);
+	OCL_CHECK(err);
+
+
+
+
+
+
 
 	clFinish(queue);
 
@@ -181,7 +633,7 @@ void run1(int N)
 	printf("oclTime = %lf (s)\n", gpuTime );
 
 
-	printf("sum = %f\n", sum[0]);
+	//check_1d_f(sum, 1);
 
 #ifdef DEBUG
 	//puts("C");
@@ -192,8 +644,9 @@ void run1(int N)
 
 	// free
 	clReleaseMemObject(A_d);	
-	clReleaseMemObject(C_d);
+	//clReleaseMemObject(C_d);
 	clReleaseMemObject(sum_d);
+	clReleaseMemObject(intersum_d);
 
 
 	clReleaseProgram(program);
@@ -210,11 +663,15 @@ void run1(int N)
 
 
 	free(A);
-	free(C);
+	//free(C);
 	free(sum);
 
 	return;
 }
+
+
+
+
 
 //---------------
 // use shared mem
--- Reduction/2D/kernel.cl
+++ Reduction/2D/kernel.cl
@@ -1,40 +1,95 @@
 //#pragma OPENCL EXTENSION all : enable
 #pragma OPENCL EXTENSION cl_amd_printf : enable
-#pragma OPENCL EXTENSION cl_khr_int64_base_atomics : enable
 
-/*
-inline void AtomicAdd(volatile __global float *source, const float operand) {
-	union {
-		unsigned int intVal;
-		float floatVal;
-	} newVal;
 
-	union {
-		unsigned int intVal;
-		float floatVal;
-	} prevVal;
-
-	do {
-		prevVal.floatVal = *source;
-		newVal.floatVal = prevVal.floatVal + operand;
-	} while (atomic_cmpxchg((volatile __global unsigned int *)source, prevVal.intVal, newVal.intVal) != prevVal.intVal);
-}
-*/
+#define TILE 64
 
 __kernel void reduction_1(
 		__global float *A, 
-		__global float *C, 
+		//__global float *C, 
 		__global float *sum,
 		__local  volatile float *sm,
-		const int N)
+		const int N,
+		const int blk)
 {
 	size_t gid = get_global_id(0); 
 	size_t lid = get_local_id(0); 
+	size_t blk_id =  get_group_id(0);
+	int k;
+	float value;
 
 	if ( gid < N )
 	{
-		float value = 0.f;
-		int k;
+		value = 0.f;
+		for ( k = 0; k < N; ++k) 
+		{
+			value += A[gid*N + k];
+		}
+
+		//printf("value[%d] = %f\n", lid, value);
+		sm[lid] = value;
+		//printf("sm[%d] = %f\n", lid, sm[lid]);
+	}else {
+		sm[lid] = 0.f;
+
+	}
+
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	if ( gid < N )
+	{
+		if ( lid < 32) 
+		{
+			sm[lid] += sm[lid + 32];
+			sm[lid] += sm[lid + 16];
+			sm[lid] += sm[lid +  8];
+			sm[lid] += sm[lid +  4];
+			sm[lid] += sm[lid +  2];
+			sm[lid] += sm[lid +  1];
+		}
+
+		if(lid == 0) 
+		{
+			//printf("%f\n", sm[0]);
+			//printf("sum[0] = %f\n", sum[lid]);
+			sum[blk_id] = sm[0];
+		}
+	}
+
+	barrier(CLK_GLOBAL_MEM_FENCE);
+
+	if( gid == 0 ) 
+	{
+		// accumulate the first blk samples	
+		value = 0.f;
+		for ( k = 0 ; k < blk; ++k ) 
+		{
+			value += sum[k];	
+		}
+		sum[blk] = value;
+	}
+
+}
+
+
+
+__kernel void reduction_2a(
+		__global float *A, 
+		// __global float *C, 
+		__global float *sum,
+		__local  volatile float *sm,
+		const int N,
+		const int blk)
+{
+	size_t gid = get_global_id(0); 
+	size_t lid = get_local_id(0); 
+	size_t blk_id =  get_group_id(0);
+	int k;
+	float value;
+
+	if ( gid < N )
+	{
+		value = 0.f;
 		for ( k = 0; k < N; ++k) 
 		{
 			value += A[gid*N + k];
@@ -48,7 +103,49 @@ __kernel void reduction_1(
 
 	barrier(CLK_LOCAL_MEM_FENCE);
 
-	if ( lid < 32) {
+	if ( gid < N )
+	{
+		if ( lid < 32) 
+		{
+			sm[lid] += sm[lid + 32];
+			sm[lid] += sm[lid + 16];
+			sm[lid] += sm[lid +  8];
+			sm[lid] += sm[lid +  4];
+			sm[lid] += sm[lid +  2];
+			sm[lid] += sm[lid +  1];
+		}
+
+		if(lid == 0) 
+		{
+			//printf("%f\n", sm[0]);
+			//printf("sum[0] = %f\n", sum[lid]);
+			sum[blk_id] = sm[0];
+		}
+	}
+
+}
+
+__kernel void reduction_2b(
+		__global float *sum,
+		__local  volatile float *sm,
+		const int N,
+		const int blk)
+{
+	size_t gid = get_global_id(0); 
+	size_t lid = get_local_id(0); 
+	size_t blk_id =  get_group_id(0);
+
+	if( gid < blk ) 
+	{
+		sm[lid] = sum[gid];
+	}else{
+		sm[lid] = 0.f; 
+	}
+
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	if ( lid < 32) 
+	{
 		sm[lid] += sm[lid + 32];
 		sm[lid] += sm[lid + 16];
 		sm[lid] += sm[lid +  8];
@@ -57,13 +154,115 @@ __kernel void reduction_1(
 		sm[lid] += sm[lid +  1];
 	}
 
-	if(lid == 0) {
-		//printf("%f\n", sm[0]);
-		//printf("sum[0] = %f\n", sum[0]);
-		AtomicAdd(sum,sm[0]);
-		//float x = sm[0];
+	if(lid == 0) 
+	{
+		sum[blk] = sm[0];
 	}
 }
+
+
+__kernel void reduction_3a(
+		__global float *A, 
+		__global float *intersum,
+		__local  volatile float *sm, // TILE
+		const int N,
+		const int blks)
+{
+	int gx = get_global_id(0); // row
+
+	//int gy = get_global_id(1); // col
+	//int bx = get_group_id(0);
+	//int by = get_group_id(1);
+	//int tx = get_local_id(0);
+
+	int ty = get_local_id(1);
+
+	int m;	
+	float v = 0.f;
+
+	#pragma unroll
+	for ( m = 0; m < blks; ++m)
+	{
+		v += A[gx * N + m * TILE + ty];	
+	}
+
+	sm[ty] = v;
+
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	if ( ty < 32) 
+	{
+		sm[ty] += sm[ty + 32];
+		sm[ty] += sm[ty + 16];
+		sm[ty] += sm[ty +  8];
+		sm[ty] += sm[ty +  4];
+		sm[ty] += sm[ty +  2];
+		sm[ty] += sm[ty +  1];
+	}
+
+	if(ty == 0) 
+	{
+		intersum[gx] = sm[0];
+	}
+
+}
+
+__kernel void reduction_3b(
+		__global float *intersum,
+		__global float *sum,
+		__local  volatile float *sm, // 64 
+		const int N,
+		const int blks)
+{
+	int gid = get_global_id(0); 
+	int lid = get_local_id(0);
+	int blkid = get_group_id(0);
+	int offset = blkid * TILE;
+
+	// read with stride of TILE
+	intersum[gid+offset] += intersum[gid+offset+TILE];
+
+	// copy to shared memory
+	sm[lid] = intersum[gid+offset];
+
+	barrier(CLK_LOCAL_MEM_FENCE);
+
+	// reduction
+	if ( lid < 32) 
+	{
+		sm[lid] += sm[lid + 32];
+		sm[lid] += sm[lid + 16];
+		sm[lid] += sm[lid +  8];
+		sm[lid] += sm[lid +  4];
+		sm[lid] += sm[lid +  2];
+		sm[lid] += sm[lid +  1];
+	}
+
+	if( lid == 0) 
+	{
+		intersum[offset*2] = sm[0];
+	}
+
+	barrier(CLK_GLOBAL_MEM_FENCE);
+
+	if(gid == 0){
+		int k;
+		float v=0.f;
+		#pragma unroll
+		for ( k = 0 ; k < blks; ++k)
+		{
+			v += intersum[k*TILE*2];	
+		}
+		sum[0] = v;
+	}
+
+}
+
+
+
+
+
+
 
 /*
    __kernel void matrix_mul(
